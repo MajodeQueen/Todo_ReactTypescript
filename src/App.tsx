c4d462861eeb505a87ae4bc706b85f26a3fb0dac
Nakyonyimaj <majorinekayondo@gmail.com>
2023-03-27 15:13:45 +0300

    fix- renamed input component to a proper typescript filed tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import Inputfield from './components/input';
+import Inputfield from './components/Inputfield';
+
 
 const App: React.FC = () => {
+  const [todo,setTodo]=useState<string>("")
   return (
     <div className="bg-blue-500 w-full min-h-screen">
       <div className="flex items-center justify-center pt-4 heading-font text-3xl text-white">
         REACT TYPESCRIPT TODO APP
       </div>
       <div className='flex items-center justify-center'>
-        <Inputfield />
+        <Inputfield todo={todo} setTodo={setTodo} />
       </div>
     </div>
   );
